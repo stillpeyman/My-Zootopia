9f8917418e341c9d3604e06fe8d13942f4579544
Peyman <still.peyman@gmail.com>
2025-05-18 13:29:31 +0200

    Missing fields not printing, message informing user HTML generated implemented.

--- main.py
+++ main.py
@@ -67,15 +67,34 @@ def serialize_animal(animal):
     """
     output = ''
     output += ' <li class="cards__item">\n'
-    output += '     <div class="card__title">{}</div>\n'.format(animal.get('name', 'Unknown'))
+
+    if animal.get('name'):
+        output += '     <div class="card__title">{}</div>\n'.format(animal['name'])
     output += '     <div class="card__text">\n'
     output += '         <ul>\n'
-    output += '             <li><strong>Scientific Name:</strong> {}</li>\n'.format(animal.get('taxonomy', {}).get('scientific_name', 'Unknown'))
-    output += '             <li><strong>Type:</strong> {}</li>\n'.format(animal.get('characteristics', {}).get('type', 'Unknown'))
-    output += '             <li><strong>Skin Type:</strong> {}</li>\n'.format(animal.get('characteristics', {}).get('skin_type', 'Unknown'))
-    output += '             <li><strong>Diet:</strong> {}</li>\n'.format(animal.get('characteristics', {}).get('diet', 'Unknown'))
-    output += '             <li><strong>Location:</strong> {}</li>\n'.format(animal.get('locations', [None])[0] or 'Unknown')
-    output += '             <li><strong>Slogan:</strong> {}</li>\n'.format(animal.get('characteristics', {}).get('slogan', 'Unknown'))
+
+    taxonomy = animal.get('taxonomy', {})
+    characteristics = animal.get('characteristics', {})
+    locations = animal.get('locations', [])
+
+    if taxonomy.get('scientific_name'):
+        output += '             <li><strong>Scientific Name:</strong> {}</li>\n'.format(taxonomy['scientific_name'])
+    
+    if characteristics.get('type'):
+        output += '             <li><strong>Type:</strong> {}</li>\n'.format(characteristics['type'])
+    
+    if characteristics.get('skin_type'):
+        output += '             <li><strong>Skin Type:</strong> {}</li>\n'.format(characteristics['skin_type'])
+    
+    if characteristics.get('diet'):
+        output += '             <li><strong>Diet:</strong> {}</li>\n'.format(characteristics['diet'])
+    
+    if locations and locations[0]:
+        output += '             <li><strong>Location:</strong> {}</li>\n'.format(locations[0])
+    
+    if characteristics.get('slogan'):
+        output += '             <li><strong>Slogan:</strong> {}</li>\n'.format(characteristics['slogan'])
+
     output += '         </ul>\n'
     output += '     </div>\n'
     output += ' </li>\n'
@@ -101,10 +120,11 @@ def merge_template_with_data(template_str, animal_info_str):
 
 def write_to_file(file_path, content):
     """
-    Write the final HTM content as a file.
+    Write the final HTML content as a file.
     """
     with open(file_path, 'w', encoding='utf-8') as file:
         file.write(content)
+    print("HTML file successfully generated :)")
 
 
 def main():
